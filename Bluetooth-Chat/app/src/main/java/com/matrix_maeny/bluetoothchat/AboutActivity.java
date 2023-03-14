7d85d46640cafbe79bb14a8de3483d6304a12f60
D Karthik reddy <124157018@sastra.ac.in>
2023-03-14 23:31:38 +0530

    dw

--- Bluetooth-Chat/app/src/main/java/com/matrix_maeny/bluetoothchat/AboutActivity.java
+++ Bluetooth-Chat/app/src/main/java/com/matrix_maeny/bluetoothchat/AboutActivity.java
@@ -2,8 +2,15 @@ package com.matrix_maeny.bluetoothchat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
-
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 public class AboutActivity extends AppCompatActivity {
 
     @Override
---
+++ Bluetooth-Chat/app/src/main/java/com/matrix_maeny/bluetoothchat/Wallet.java
@@ -0,0 +1,14 @@
+package com.matrix_maeny.bluetoothchat;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Wallet  extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.wallet_activity);
+        }
+}
