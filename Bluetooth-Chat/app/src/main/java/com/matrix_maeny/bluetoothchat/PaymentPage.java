b8b1a2af7a1d7518c4f1f9abc6b5c0e47723025f
D Karthik reddy <124157018@sastra.ac.in>
2023-03-14 22:14:30 +0530

    a
       a

--- Bluetooth-Chat/app/src/main/java/com/matrix_maeny/bluetoothchat/PaymentPage.java
+++ Bluetooth-Chat/app/src/main/java/com/matrix_maeny/bluetoothchat/PaymentPage.java
@@ -1,4 +1,14 @@
 package com.matrix_maeny.bluetoothchat;
 
-public class PaymentPage {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_layout);
+    }
 }
