a49561270d93ee0e4337ea099c9ee53b977fbd2f
D Karthik reddy <124157018@sastra.ac.in>
2023-03-25 22:57:38 +0530

    app with browser
    added the browser to android app

--- Bluetooth-Chat/app/src/main/java/com/matrix_maeny/bluetoothchat/AboutActivity.java
+++ Bluetooth-Chat/app/src/main/java/com/matrix_maeny/bluetoothchat/AboutActivity.java
@@ -3,7 +3,11 @@ package com.matrix_maeny.bluetoothchat;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.io.FileInputStream;
@@ -26,6 +30,41 @@ public class AboutActivity extends Activity {
         setContentView(R.layout.activity_about);
         textmsg = (TextView)findViewById(R.id.textView7);
         ReadBtn();
+
+        WebView mywebview = (WebView) findViewById(R.id.webView);
+        mywebview.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        mywebview.loadUrl("https://www.sastra.edu/");
+        // this will enable the javascript.
+        mywebview.getSettings().setJavaScriptEnabled(true);
+        mywebview.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url);
+                return false; // then it is not handled by default action
+            }
+
+        });
+        mywebview.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                    WebView webView = (WebView) v;
+
+                    switch(keyCode) {
+                        case KeyEvent.KEYCODE_BACK:
+                            if (webView.canGoBack()) {
+                                webView.goBack();
+                                return true;
+                            }
+                            break;
+                    }
+                }
+
+                return false;
+            }
+        });
+
         }
 
     // Read text from file
@@ -50,5 +89,13 @@ public class AboutActivity extends Activity {
             Toast.makeText(getBaseContext(), "file :"+e,
                     Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void WebLoading(){
+        WebView mywebview = (WebView) findViewById(R.id.webView);
+        mywebview.loadUrl("https://www.sastra.edu/");
+        // this will enable the javascript.
+        mywebview.getSettings().setJavaScriptEnabled(true);
+
     }
 }
